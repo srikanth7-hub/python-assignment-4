47ef4f4e94050b33ae419bab64d059a8d70d62d3
srikanth7-hub <68545610+srikanth7-hub@users.noreply.github.com>
2020-07-23 16:42:31 +0530

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+str1="what we think we become"
+str1.count("we",0,15)
+
+
+# In[3]:
+
+
+str1[0:15].count("we")
+
+
+# 
+
+# In[5]:
+
+
+str1.find("we")
+
+
+# In[6]:
+
+
+str1[5]
+
+
+# In[7]:
+
+
+a="what we think we become"
+a.rfind("we")
+
+
+# In[9]:
+
+
+a.find("we")
+
+
+# In[12]:
+
+
+a.upper()
+
+
+# In[13]:
+
+
+a.title()
+
+
+# In[14]:
+
+
+a.islower()
+
+
+# In[15]:
+
+
+a.isupper()
+
+
+# In[ ]:
+
+
+
+
